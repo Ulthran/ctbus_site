062f691a5b506e8909bdb07f89ff79e4a13f59d0
Ubuntu <ubuntu@ip-172-31-16-17.ec2.internal>
2021-03-14 07:10:17 +0000

    Whoops forgot this

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,8 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
+Route::get('/login', function () {
+    return view('login');
+});
+
 require __DIR__.'/auth.php';
