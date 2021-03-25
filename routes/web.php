9feeea1819d83ac1df327b87dadbcae45768a88f
Ulthran <ctbushman@gmail.com>
2021-03-25 19:08:12 +0000

    Fixed routing

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('pages/home');
 });
 
 Route::get('/resume', function () {
-    return view('resume');
+    return view('pages/resume');
 });
 
 Route::get('/dashboard', function () {
