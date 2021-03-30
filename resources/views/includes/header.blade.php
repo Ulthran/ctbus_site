41e078401bfd77da819a126f1893a412dd1809d1
Ulthran <ctbushman@gmail.com>
2021-03-29 22:49:52 -0400

    Quick formatting change

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,7 +1,20 @@
+<style>
+.navbar-inner {
+    border-bottom: 1px solid #000;
+    text-align: center;
+}
+
+ul#nav li {
+    display: inline;
+    font-size: large;
+
+}
+</style>
+
 <div class="navbar">
    <div class="navbar-inner">
        <!--<a id="logo" href="/">ctbus</a>-->
-       <ul class="nav">
+       <ul id="nav">
            <li><a href="/">Home</a></li>
            <li><a href="/resume">Resume</a></li>
        </ul>
