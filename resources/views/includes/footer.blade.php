3a1f0db65429a9d1c81139446d2081187df776e8
Ulthran <ctbushman@gmail.com>
2021-03-31 00:07:35 +0000

    Resume page looking good

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -10,10 +10,17 @@
     display: flex;
     justify-content: space-between;
     width: 90%;
+    padding: 25px;
+    padding-bottom: 10px;
 }
 .footer-note {
     display: flex;
     width: 90%;
+    justify-content: flex-end;
+    padding-bottom: 10px;
+}
+.mailto-link {
+    color: #00F;
 }
 </style>
 
@@ -85,6 +92,6 @@
         <p id="bottom-dash">-</p>
     </div>
     <div class="footer-note">
-        
+       <p>Please direct any inquiries, bug reports, or interesting anecdotes to <a class="mailto-link" href="mailto:ctbushman@gmail.com">ctbushman@gmail.com</a>.</p> 
     </div>
 </div>
