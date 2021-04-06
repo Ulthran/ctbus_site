edb55d11e33639ea1a13f124886a025f4dd11f33
Ulthran <ctbushman@gmail.com>
2021-04-06 19:46:05 +0000

    Getting AWS sddk up and running

--- resources/views/pages/projects.blade.php
+++ resources/views/pages/projects.blade.php
@@ -1,5 +1,31 @@
 @extends('layouts.app')
 
 @section('content')
-PROJECTS
+<?php
+    require "/var/www/ctbus_site/vendor/autoload.php";
+    use Aws\S3\S3Client;
+    use Aws\Exception\AwsException;
+
+    $sharedConfig = [
+        'region' => 'us-east-1',
+	'version' => 'latest'
+    ];
+
+    $sdk = new Aws\Sdk($sharedConfig);
+    $client = $sdk->createS3();
+
+    $resumeUrl = $client->getObjectUrl(getenv('AWS_BUCKET'), 'resume.pdf', '5 minutes');
+    $result = $client->getObject([
+        'Bucket' => getenv('AWS_BUCKET'),
+	'Key' => 'resume.pdf'
+    ]);
+?>
+
+<style>
+
+</style>
+
+<div id="container">
+    <a href="<?php echo $resumeUrl ?>" download>LINK</a>
+</div>
 @stop
