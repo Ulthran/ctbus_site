55c2c2da525820af863a5902bbbb366f248deff7
ctbus <ctbushman@gmail.com>
2023-11-24 23:53:16 -0500

    Catch non-standard return code 999

--- tests/e2e/test_links.py
+++ tests/e2e/test_links.py
@@ -11,7 +11,7 @@ def test_broken_links(setup_chrome):
         url = link.get_attribute("href")
         try:
             response_code = int(requests.head(url).status_code)
-            if response_code >= 200 and response_code < 300:
+            if (response_code >= 200 and response_code < 300) or response_code == 999:
                 assert True
             else:
                 assert False, f"Bad link: {url}"
