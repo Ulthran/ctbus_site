2675559e396ec26bdc1e0b3722a8a93e794fc7a4
Ulthran <ctbushman@gmail.com>
2023-11-29 22:04:20 -0500

    Add sitemapper

--- app/app.py
+++ app/app.py
@@ -1,19 +1,14 @@
 import os
 from flask import Flask, render_template, send_from_directory
+from flask_sitemapper import Sitemapper
+from app import project_pages
 from app.data_utils import get_chess_stats
 
-# from app.S3 import S3
-
-print("Starting app...")
+sitemapper = Sitemapper()
 app = Flask(__name__)
 app.secret_key = os.urandom(12)
-
-print("Getting environment variables...")
-# bucket = os.environ.get("BUCKET", "ctbus-site-db")
-# s3 = S3(bucket)
-# ENV = s3.get_env()
+sitemapper.init_app(app)
 ENV = {"CDN_URL": "https://d2w4s6xs8769uj.cloudfront.net"}
-print("Ready to go!")
 
 
 @app.route("/favicon.ico")
@@ -25,6 +20,11 @@ def favicon():
     )
 
 
+@sitemapper.include(
+    lastmod="2023-11-29",
+    changefreq="monthly",
+    priority=1.0,
+)
 @app.route("/")
 def index():
     return render_template("index.html", cdn_url=ENV.get("CDN_URL", ""))
@@ -35,26 +35,47 @@ def page_not_found(e):
     return render_template("error.html", cdn_url=ENV.get("CDN_URL", "")), 404
 
 
+@sitemapper.include(
+    lastmod="2023-11-29",
+    changefreq="monthly",
+    priority=0.9,
+)
 @app.route("/pcmp")
 def pcmp():
     return render_template("pcmp.html", cdn_url=ENV.get("CDN_URL", ""))
 
 
+@sitemapper.include(
+    lastmod="2023-11-29",
+    changefreq="monthly",
+    priority=0.9,
+)
 @app.route("/music")
 def music():
     return render_template("music.html", cdn_url=ENV.get("CDN_URL", ""))
 
 
+@sitemapper.include(
+    lastmod="2023-11-29",
+    changefreq="monthly",
+    priority=0.9,
+)
 @app.route("/projects")
 def projects():
     return render_template("projects.html", cdn_url=ENV.get("CDN_URL", ""))
 
 
+@sitemapper.include(url_variables={"project": project_pages()})
 @app.route("/projects/<project>")
 def project(project):
     return render_template(f"projects/{project}.html", cdn_url=ENV.get("CDN_URL", ""))
 
 
+@sitemapper.include(
+    lastmod="2023-11-29",
+    changefreq="monthly",
+    priority=0.9,
+)
 @app.route("/sports")
 def sports():
     return render_template(
@@ -62,14 +83,29 @@ def sports():
     )
 
 
+@sitemapper.include(
+    lastmod="2023-11-29",
+    changefreq="monthly",
+    priority=0.9,
+)
 @app.route("/certifications")
 def certifications():
     return render_template("certifications.html", cdn_url=ENV.get("CDN_URL", ""))
 
 
+@sitemapper.include(
+    lastmod="2023-11-29",
+    changefreq="monthly",
+    priority=0.9,
+)
 @app.route("/favorite-number")
 def favorite_number():
     return render_template("favorite-number.html", cdn_url=ENV.get("CDN_URL", ""))
+
+
+@app.route("/sitemap.xml")
+def r_sitemap():
+    return sitemapper.generate()
 
 
 if __name__ == "__main__":
