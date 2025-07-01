e396597f44452fa8234a49466b6e40fe8d44a122
Charlie <ctbushman@gmail.com>
2025-07-01 10:24:11 -0400

    Show loading indicator while fetching blog posts

--- vue-frontend/src/views/BlogPost.vue
+++ vue-frontend/src/views/BlogPost.vue
@@ -2,20 +2,31 @@
 import { useRoute } from 'vue-router'
 import { ref, watch } from 'vue'
 
+const loading = ref(true)
+const notFound = ref(false)
+
 const route = useRoute()
 const component = ref(null)
 
 async function loadComponent() {
+  loading.value = true
+  notFound.value = false
+  component.value = null
   const name = route.params.slug
   try {
     component.value = await window['vue3-sfc-loader'].loadModule(
       `${window.postsPath}/${name}.vue`,
       window.loaderOptions,
     )
+    if (!component.value) {
+      notFound.value = true
+    }
   } catch (e) {
     console.error('Error loading component:', e)
     component.value = null
+    notFound.value = true
   }
+  loading.value = false
 }
 
 watch(
@@ -28,6 +39,9 @@ watch(
 </script>
 
 <template>
-  <component :is="component" v-if="component" />
-  <v-container v-else>Post not found.</v-container>
+  <v-container class="py-8 d-flex justify-center" v-if="loading">
+    <v-progress-circular indeterminate />
+  </v-container>
+  <component :is="component" v-else-if="component" />
+  <v-container v-else-if="notFound">Post not found.</v-container>
 </template>
