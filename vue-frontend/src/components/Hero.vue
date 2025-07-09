28321041d68d2bd330a7939471d3f0b5b163e418
Charlie <ctbushman@gmail.com>
2025-07-09 17:23:58 -0400

    fix(hero): allow width customization

--- vue-frontend/src/components/Hero.vue
+++ vue-frontend/src/components/Hero.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container class="mt-8 text-center">
     <v-row justify="center">
-      <v-col cols="12" md="6">
+      <v-col cols="12" :md="md">
         <h1 class="text-h4 font-weight-bold mb-2">{{ title }}</h1>
         <p v-html="subtitle" class="text-subtitle-1"></p>
         <slot />
@@ -13,5 +13,9 @@
 defineProps({
   title: String,
   subtitle: String,
+  md: {
+    type: [Number, String],
+    default: 6,
+  },
 });
 </script>
