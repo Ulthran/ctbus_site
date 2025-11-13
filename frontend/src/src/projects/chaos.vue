b6cb86aa6400351e5d5fd268dccd0d01cd51f55a
Ulthran <ctbushman@gmail.com>
2025-11-13 12:46:30 -0500

    Add Xylblox and chaos (jupyter)

--- frontend/src/src/projects/chaos.vue
+++ frontend/src/src/projects/chaos.vue
@@ -2,7 +2,7 @@
 import ProjectHero from "../components/ProjectHero.vue";
 const projects = window.projects;
 
-const info = projects["ctbus-finance"];
+const info = projects["chaos"];
 </script>
 
 <template>
@@ -16,14 +16,7 @@ const info = projects["ctbus-finance"];
     <v-card class="mb-4">
       <v-card-title>Overview</v-card-title>
       <v-card-text>
-        An example of using Beancount for personal finance tracking. Through the magic of double-entry accounting, I can see all of the money that moves through me and my accounts.
-      </v-card-text>
-    </v-card>
-
-    <v-card class="mb-4">
-      <v-card-title>Dev</v-card-title>
-      <v-card-text>
-        The core of the project is Beancount, a double-entry accounting system that uses a text-based input file to track accounts and transactions. I use Python scripts and custom importers to pull in data from bank accounts, credit cards, investment accounts, etc. All of this can then be seen in a local dashboard run with Fava.
+        This one started out as a demo for a guest lecture on snakemake but I couldn't get the JS-based kernel to work with all of the shell commands snakemake is built around, so I turned to recreating all my favorite chaotic systems and fractals instead (and ended up showing the snakemake demo on my machine, no interactive component :(). I'm always coming back to this and adding more systems or updating existing ones with new features and documentation. There are always crazy new ways to visualize chaos!
       </v-card-text>
     </v-card>
 
@@ -32,15 +25,19 @@ const info = projects["ctbus-finance"];
       <v-card-text>
         <v-row>
           <v-col cols="auto">
-            <router-link to="/blog/ctbus-finance">
+            <a
+              href="https://jupyter.charliebushman.com"
+              target="_blank"
+              title="Jupyter Site"
+            >
               <v-btn variant="text" icon>
                 <i class="fas fa-globe"></i>
               </v-btn>
-            </router-link>
+            </a>
           </v-col>
           <v-col cols="auto">
             <a
-              href="https://github.com/Ulthran/ctbus_finance"
+              href="https://github.com/Ulthran/ctbus_site/tree/master/jupyter"
               target="_blank"
               title="GitHub Repo"
             >
---
+++ frontend/src/src/projects/xylblox.vue
@@ -0,0 +1,53 @@
+<script setup>
+import ProjectHero from "../components/ProjectHero.vue";
+const projects = window.projects;
+
+const info = projects["xylblox"];
+</script>
+
+<template>
+  <ProjectHero
+    :title="info.title"
+    :subtitle="info.subtitle"
+    :tags="info.tags"
+    :img="`ASSETS_BASE_URL/images/${info.image}`"
+  />
+  <v-container>
+    <v-card class="mb-4">
+      <v-card-title>Overview</v-card-title>
+      <v-card-text>
+        This is as much a passion project as a viable business idea. I've spent way too many hours through my life playing Minecraft and multiplayer is always a pain (especially if you're cheap and don't want to pay for monthly hosting plans). Xylblox started simply as me building a robust Minecraft hosting architecture on AWS for me and some friends to play. As I did that, I realized there is a real opportunity to create a product that doesn't exist on the Minecraft hosting market: a true pay-per-usage model. Everyone else charges flat monthly fees that rely on overprovisioning and customer inertia to make money. I can build something that is more cost effective and doesn't prey on customers's absentmindedness. It's still early days, but I'm excited to keep building this out.
+      </v-card-text>
+    </v-card>
+
+    <v-card>
+      <v-card-title>Links</v-card-title>
+      <v-card-text>
+        <v-row>
+          <v-col cols="auto">
+            <a
+              href="https://xylblox.com"
+              target="_blank"
+              title="Xylblox Site"
+            >
+              <v-btn variant="text" icon>
+                <i class="fas fa-globe"></i>
+              </v-btn>
+            </a>
+          </v-col>
+          <v-col cols="auto">
+            <a
+              href="https://github.com/Ulthran/minecraft_for_all"
+              target="_blank"
+              title="GitHub Repo"
+            >
+              <v-btn variant="text" icon>
+                <i class="fab fa-github"></i>
+              </v-btn>
+            </a>
+          </v-col>
+        </v-row>
+      </v-card-text>
+    </v-card>
+  </v-container>
+</template>
